8a93a4e3187040c5fcad35434455f8c4beaf2dba
Livio Benčik <lbenc135@gmail.com>
2025-02-18 10:39:15 +0100

    Fix create_service python template

--- create_service_templates/python/hooks/pre_gen_project.py
+++ create_service_templates/python/hooks/pre_gen_project.py
@@ -1,13 +1,13 @@
 import sys
 from os import path
 
-from forge_cli.utils import load_yaml
+from forge_cli.utils import load_templated_yaml
 
 
 if path.isdir("../services/{{ cookiecutter.service_identifier }}"):
     sys.exit("A service with this identifier already exists")
 
 with open("../config/config.yaml", "r") as f:
-    data = load_yaml(f.read())
+    data = load_templated_yaml(f.read())
     if '{{ cookiecutter.service_artifact_name }}' in data['services']:
         sys.exit("A service with this identifier already exists")
